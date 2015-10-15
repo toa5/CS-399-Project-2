b3a4c43d8bd3e18ebe7ad008ec50a17eec03c2e0
toa5 <toa5@nau.edu>
2015-10-14 17:55:14 -0700

    Completed Game

--- GameActivity.java
+++ GameActivity.java
@@ -9,40 +9,87 @@ import android.content.ClipDescription;
 import android.view.DragEvent;
 import android.view.View;
 import android.view.View.OnDragListener;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
+@SuppressLint("NewApi")
 public class GameActivity extends AnagramActivity 
 {
+	private final static Random random = new Random();
 	private final ArrayList<AnagramButton> buttons = new ArrayList<AnagramButton>();
-    private final ArrayList<String> puzzles = new ArrayList<String>();
-    private final ArrayList<String> solutions = new ArrayList<String>();
-    private final Random rand = new Random();
+	private final static int[][] strings = 
+	{
+		{R.string.p1, R.string.s1},
+		{R.string.p2, R.string.s2},
+		{R.string.p3, R.string.s3},
+		{R.string.p4, R.string.s4},
+		{R.string.p5, R.string.s5},
+		{R.string.p6, R.string.s6},
+		{R.string.p7, R.string.s7},
+		{R.string.p8, R.string.s8},
+		{R.string.p9, R.string.s9},
+		{R.string.p10, R.string.s10},
+		{R.string.p11, R.string.s11},
+		{R.string.p12, R.string.s12},
+		{R.string.p13, R.string.s13},
+		{R.string.p14, R.string.s14},
+	};
+	private String word, solution;
 	
 	public GameActivity() 
 	{	super(R.layout.activity_game);}
 	
-	@SuppressLint("NewApi")
 	@Override
 	public void onCreate()
 	{
+		setRandomWord();
+		
 		LinearLayout layout = (LinearLayout) this.findViewById(R.id.wordLayout);
 		layout.setOnDragListener(new DragListener());
-        setArrayLists();
-        int index = rand.nextInt(14);
-		String word = puzzles.get(index);
-        char c;
 		for(int i = 0; i < word.length(); ++i)
 		{
-			c = word.charAt(i);
+			char c = word.charAt(i);
 			if(Character.isLetter(c))
 			{
 				AnagramButton button = new AnagramButton(this, c, i);
 				layout.addView(button);
-                button.setPadding(0,0,0,0); // this is important to show the entire letter
 				buttons.add(button);
 			}
 		}
 		layout.setWeightSum(buttons.size());
+	}
+	
+	void setRandomWord()
+	{
+		int wordIndex = random.nextInt(strings.length);
+		word = getString(strings[wordIndex][0]);
+		word = word.replaceAll("\\s", "");
+		solution = getString(strings[wordIndex][1]);
+		solution = solution.replaceAll("\\s", "");
+	}
+	
+	void checkSolution()
+	{
+		StringBuilder str = new StringBuilder(buttons.size());
+		for(Button button : buttons)
+			str.append(button.getText());
+		
+		//anagram solved
+		if(solution.equalsIgnoreCase(str.toString()))
+			endGame();
+	}
+	
+	void endGame()
+	{
+		LinearLayout layout = (LinearLayout) this.findViewById(R.id.wordLayout);
+		layout.setOnDragListener(null);
+		
+		for(Button button : buttons)
+			button.setOnTouchListener(null);
+		
+		TextView text = (TextView) this.findViewById(R.id.gameText);
+		text.setText(getString(R.string.successMessage));
 	}
 	
 	@SuppressLint("NewApi")
@@ -68,48 +115,15 @@ public class GameActivity extends AnagramActivity
 					LinearLayout layout = (LinearLayout) view;
 					layout.removeAllViews();
 					for(AnagramButton button : buttons)
-                        layout.addView(new AnagramButton(button));
+						layout.addView(new AnagramButton(button));
 					buttons.clear();
-					for(int i = 0, n = layout.getChildCount(); i < n; ++i) {
-                        buttons.add((AnagramButton) layout.getChildAt(i));
-                        buttons.get(buttons.size()-1).setPadding(0,0,0,0); // uncover letters
-                    }
-					layout.postInvalidate();
+					for(int i = 0, n = layout.getChildCount(); i < n; ++i)
+						buttons.add((AnagramButton) layout.getChildAt(i));
+					checkSolution();
 					return true;
 				default:
 					return false;
 			}
 		}
 	}
-
-    private void setArrayLists(){
-        puzzles.add(0, "saucy tea dot");
-        solutions.add(0, "taco tusday");
-        puzzles.add(1, "auto hub wits");
-        solutions.add(1, "without a bus");
-        puzzles.add(2, "nice dong thievery");
-        solutions.add(2, "code in everything");
-        puzzles.add(3, "banks alert");
-        solutions.add(3, "blank stare");
-        puzzles.add(4,"abolish tennis gipsy");
-        solutions.add(4, "is anything possible");
-        puzzles.add(5, "rave crust erie");
-        solutions.add(5, "vice treasurer");
-        puzzles.add(6, "goth trip");
-        solutions.add(6, "top right");
-        puzzles.add(7, "oak boo dog");
-        solutions.add(7, "a good book");
-        puzzles.add(8, "berry bum guy");
-        solutions.add(8, "buy my burger");
-        puzzles.add(9, "beehive sod tom");
-        solutions.add(9, "move the bodies");
-        puzzles.add(10, "sassy chosen pill");
-        solutions.add(10, "physical lessons");
-        puzzles.add(11, "solar dusk");
-        solutions.add(11, "dark souls");
-        puzzles.add(12, "mad meal slag");
-        solutions.add(12, "small damage");
-        puzzles.add(13, "booth rouse");
-        solutions.add(13,"robot house");
-    }
 }
